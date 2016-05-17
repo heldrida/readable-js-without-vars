45ae0e97f7d4d5120dc37e7721ea4c8b7634c7ce
Helder <helder@lamoulade.com>
2016-05-17 15:49:35 +0100

    Created the imperative approach

--- js/app.js
+++ js/app.js
@@ -1,7 +1,52 @@
 'use strict';
 
-var foo = function foo() {
-	console.log("Hello world!!");
+var penguins = [{
+	age: 12,
+	male: true
+}, {
+	age: 32,
+	male: false
+}, {
+	age: 53,
+	male: true
+}];
+
+// imperative approach
+var avgAgeOfMales = function avgAgeOfMales(penguins) {
+
+	var total = 0;
+	var num = 0;
+
+	var _iteratorNormalCompletion = true;
+	var _didIteratorError = false;
+	var _iteratorError = undefined;
+
+	try {
+		for (var _iterator = penguins[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+			var penguin = _step.value;
+
+
+			if (penguin.male) {
+				total += penguin.age;
+				num++;
+			}
+		}
+	} catch (err) {
+		_didIteratorError = true;
+		_iteratorError = err;
+	} finally {
+		try {
+			if (!_iteratorNormalCompletion && _iterator.return) {
+				_iterator.return();
+			}
+		} finally {
+			if (_didIteratorError) {
+				throw _iteratorError;
+			}
+		}
+	}
+
+	return total / num;
 };
 
-foo();
+console.log("Imperative approach avgAgeOfMales returns ", avgAgeOfMales(penguins));
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,33 @@
 'use strict';
 
-let foo = () => {
-	console.log("Hello world!!");
+const penguins = [{
+	age: 12,
+	male: true
+}, {
+	age: 32,
+	male: false
+}, {
+	age: 53,
+	male: true
+}];
+
+// imperative approach
+const avgAgeOfMales = (penguins) => {
+
+	let total = 0;
+	let num = 0;
+
+	for (let penguin of penguins) {
+
+		if (penguin.male) {
+			total += penguin.age;
+			num++;
+		}
+
+	}
+
+	return total / num;
+
 };
 
-foo();
+console.log("Imperative approach avgAgeOfMales returns ", avgAgeOfMales(penguins));
